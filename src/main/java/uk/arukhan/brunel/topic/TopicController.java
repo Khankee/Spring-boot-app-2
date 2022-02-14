05d443cb627f43de690899208aa3910fb690fd53
Khankee <1940930@brunel.ac.uk>
2022-02-14 19:39:02 +0000

    Added Database and upgraded TopicService.java

--- src/main/java/uk/arukhan/brunel/topic/TopicController.java
+++ src/main/java/uk/arukhan/brunel/topic/TopicController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TopicController {
@@ -19,7 +20,7 @@ public class TopicController {
 
     //GET DEFAULT METHOD OF SPECIFIC ONE
     @RequestMapping("/topics/{id}")
-    public Topic getTopic(@PathVariable String id){
+    public Optional<Topic> getTopic(@PathVariable String id){
         return topicService.getTopic(id);
     }
 
---
+++ src/main/java/uk/arukhan/brunel/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package uk.arukhan.brunel.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
